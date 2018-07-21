a9ba76f706d4e63ef1e6817cc4c08f3bbb85b23e
gadcad98 <37885704+gadcad98@users.noreply.github.com>
2018-07-20 21:17:41 -0700

    package.json added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const lettersToDisplay = require("./letter");
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const guessedLetters = [];
 const correctlyGuessedLetters = [];
-const displayGame;
+// const displayGame = lettersToDisplay;
 
 const wordsArray = [
     "javascript",
